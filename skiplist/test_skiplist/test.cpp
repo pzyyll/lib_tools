571cb5af197384d03647f18c278fefc40e984280
cygwin_czl <pzyyll@gmail.com>
2018-06-14 00:44:51 +0800

    up

--- skiplist/test_skiplist/test.cpp
+++ skiplist/test_skiplist/test.cpp
@@ -1,7 +1,12 @@
 #include "gtest/gtest.h"
+#include "skiplist.h"
 
+TEST(TestSkiplist, Skiplist) {
+    lib_tools::SkipList<int, int> skip_rank;
+    EXPECT_EQ(0, skip_rank.Lenth()) << "Skip mes.";
+}
 
 TEST(Test, Fails) {
-    EXPECT_EQ(1, 2) << "Fail mes.";
     EXPECT_EQ(1, 1) << "Success mes.";
+    EXPECT_EQ(1, 2) << "Fail mes.";
 }
