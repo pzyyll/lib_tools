49599439ccdf0eab68cb9a020657d853853718a7
cygwin_czl <pzyyll@gmail.com>
2018-06-14 00:31:20 +0800

    yes

--- skiplist/test_skiplist/auto_gen.sh
+++ skiplist/test_skiplist/auto_gen.sh
@@ -1,8 +1,12 @@
-#Gen dep
+#!/usr/bin/env sh
+# source this-script
 
-ROOT_PATH=${PWD%/*/*}
+PHY_PWD="$(pwd -P)"
+export ROOT_PATH="${PHY_PWD%/*/*}"
 
 cd ${ROOT_PATH}/googletest
 mkdir -p cmake-build-debug && cd cmake-build-debug
 
-cmake ../ -DCMAKE_INSTALL_PREFIX=./usr/ && make && make install
+cmake ../ -DCMAKE_INSTALL_PREFIX=./usr/ -DCMAKE_INSTALL_LIBDIR=lib && make && make install
+
+cd ${PHY_PWD}
