1ce3e5efab25903195ba617bd6b394ec62271c17
cygwin_czl <pzyyll@gmail.com>
2018-06-15 01:34:43 +0800

    up

--- random/random_tools.h
+++ random/random_tools.h
@@ -17,81 +17,81 @@ namespace lib_tools {
 class Random
 {
 public:
-	template <typename T = int>
-	static T RandInt(const T min, const T max) {
-		std::uniform_int_distribution<T> dist(min, max);
-		return dist(engine);
-	}
+    template <typename T = int>
+    static T RandInt(const T min, const T max) {
+        std::uniform_int_distribution<T> dist(min, max);
+        return dist(engine);
+    }
 
-	template <typename T>
-	static void Shuffle(T begin, T end) {
-		std::shuffle(begin, end);
-	}
+    template <typename T>
+    static void Shuffle(T begin, T end) {
+        std::shuffle(begin, end);
+    }
 
-	static bool BernoulliDist(double p) {
-		std::bernoulli_distribution dist(p);
-		return dist(engine);
-	}
+    static bool BernoulliDist(double p) {
+        std::bernoulli_distribution dist(p);
+        return dist(engine);
+    }
 
-	template <typename InputIt>
-	static unsigned DiscreteDist(InputIt begin, InputIt end) {
-		std::discrete_distribution<unsigned> dist(begin, end);
-		return dist(engine);
-	}
+    template <typename InputIt>
+    static unsigned DiscreteDist(InputIt begin, InputIt end) {
+        std::discrete_distribution<unsigned> dist(begin, end);
+        return dist(engine);
+    }
 
-	template <typename T>
-	static T DiscreteDist(
-		const std::vector<T> &vals,
-		const std::vector<double> &weights
-	) {
-		std::vector<double> tmp_ws = weights;
-		while (tmp_ws.size() < vals.size())
-			tmp_ws.push_back(0);
+    template <typename T>
+    static T DiscreteDist(
+        const std::vector<T> &vals,
+        const std::vector<double> &weights
+    ) {
+        std::vector<double> tmp_ws = weights;
+        while (tmp_ws.size() < vals.size())
+            tmp_ws.push_back(0);
 
-		if (vals.size() < tmp_ws.size())
-			tmp_ws.resize(vals.size());
+        if (vals.size() < tmp_ws.size())
+            tmp_ws.resize(vals.size());
 
-		unsigned idx = DiscreteDist(tmp_ws.begin(), tmp_ws.end());
+        unsigned idx = DiscreteDist(tmp_ws.begin(), tmp_ws.end());
 
-		return vals[idx];
-	}
+        return vals[idx];
+    }
 
-	//@brief 根据权重不放回随机抽样
-	//@param population 提供的抽样数据
-	//@param weights 对应抽样数据权重
-	//@param count 从提供的抽样数据中抽取的个数
-	//@return 抽样结果
+    //@brief 根据权重不放回随机抽样
+    //@param population 提供的抽样数据
+    //@param weights 对应抽样数据权重
+    //@param count 从提供的抽样数据中抽取的个数
+    //@return 抽样结果
     template <typename Set>
-	static std::vector<typename Set::value_type> RandomSample(
-		const Set &population,
-		const std::vector<double> &weights,
-		const unsigned count
-	) {
+    static std::vector<typename Set::value_type> RandomSample(
+        const Set &population,
+        const std::vector<double> &weights,
+        const unsigned count
+    ) {
         assert(population.size() == weights.size());
         assert(population.size() > 0);
         
-		std::vector<typename Set::value_type> results;
+        std::vector<typename Set::value_type> results;
 
-		std::vector<typename Set::value_type> tmp_vals(population.begin(), population.end());
-		std::vector<double> tmp_ws(weights.begin(), weights.end());
+        std::vector<typename Set::value_type> tmp_vals(population.begin(), population.end());
+        std::vector<double> tmp_ws(weights.begin(), weights.end());
 
-		for (unsigned i = 0;
-			i < count &&
-			tmp_vals.size() > 0 &&
-			tmp_ws.size() > 0;
-			++i) {
+        for (unsigned i = 0;
+            i < count &&
+            tmp_vals.size() > 0 &&
+            tmp_ws.size() > 0;
+            ++i) {
 
-			unsigned idx = DiscreteDist(tmp_ws.begin(), tmp_ws.end());
-			unsigned res_val = tmp_vals[idx];
-			results.push_back(res_val);
-			tmp_vals.erase(tmp_vals.begin() + idx);
-			tmp_ws.erase(tmp_ws.begin() + idx);
-		}
+            unsigned idx = DiscreteDist(tmp_ws.begin(), tmp_ws.end());
+            unsigned res_val = tmp_vals[idx];
+            results.push_back(res_val);
+            tmp_vals.erase(tmp_vals.begin() + idx);
+            tmp_ws.erase(tmp_ws.begin() + idx);
+        }
 
-		return results;
-	}
+        return results;
+    }
 
-	static std::mt19937 engine;
+    static std::mt19937 engine;
 };
 
 std::mt19937 Random::engine(static_cast<unsigned>(time(NULL)));
