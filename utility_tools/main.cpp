ae61dcb9f4c61d10b70a223599f35c26a6179efc
caizhili <caizhili@gzyouai.com>
2018-11-12 09:19:20 +0800

    aa

--- utility_tools/main.cpp
+++ utility_tools/main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <array>
 
 #include "utility_tools.h"
 
@@ -12,9 +13,15 @@ int main(int argc, char **argv) {
 
     vector<int> test_nums{0, 10, 20, 30, 50, 60, 100};
 
-   cout << IndexLowerBound(test_nums.begin(), test_nums.end(), 10) << endl;
-   cout << IndexLowerBoundRightOpen(test_nums.begin(), test_nums.end(), 10) << endl;
+    cout << IndexLowerBound(test_nums.begin(), test_nums.end(), 10) << endl;
+    cout << IndexLowerBoundRightOpen(test_nums.begin(), test_nums.end(), 10) << endl;
 
+    array<int, 10> arr10 = {1};
+
+    arr10.
+    for (unsigned i = 0; i < arr10.size(); ++i) {
+        cout << arr10[i] << endl;
+    }
 
     cout << "end" << endl;
     return 0;
